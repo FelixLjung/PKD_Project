e3c31a86382a28425e22cee52308940925cd7c05
David Julin <david.carl.julin@gmail.com>
2024-02-09 13:35:08 +0100

    Player fix

--- functions.ts
+++ functions.ts
@@ -3,7 +3,7 @@ import { List, Pair , list} from "./lib/list";
 
 type Army<warrior> = Array<warrior>
 
-type Player <H, T>= [H, T] | null
+type Player <H, T>= [string, List<Castle>]
 
 type warrior = {
     attack : number
