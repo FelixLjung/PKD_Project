05dcfe5575f1fb990b37a68db65b7cbf90540757
David Julin <david.carl.julin@gmail.com>
2024-02-09 11:19:50 +0100

    mormors kudde

--- game.ts
+++ game.ts
@@ -16,4 +16,19 @@ let map = [
 
 import { type MatrixGraph } from '../lib/graphs';
 
-const mormorskudde: MatrixGraph
+
+const I = true;
+const O = false;
+const mormors_kudde: MatrixGraph = {
+    size: 5,
+    adj:
+
+        [
+        [O, I, I, I, O, O, O], //0. from A 
+        [O, O, O, O, O, O, O], //1. from B
+        [I, O, O, O, O, O, I], //2. from C
+        [O, O, O, O, O, O, O], //3. from D
+        [O, O, O, I, O, O, O], //4. from E
+        ] 
+
+}
