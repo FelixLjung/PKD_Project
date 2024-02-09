c5148841d60918fd03f6aac4089c752f8c2b3383
David Julin <david.carl.julin@gmail.com>
2024-02-09 11:53:54 +0100

    dice func done

--- functions.ts
+++ functions.ts
@@ -1,9 +1,22 @@
+//Types
+
+
+
+
+
+
+
+
+// Functions
 /**
- * 
- * @param min 
- * @param max 
- * @returns 
+ * Chooses a random number between [min] and [max].
+ * @param min is a number. Represents the lowest number on the die
+ * @param max is a {number}. Represents the hightes number on the die
+ * @returns a random number / integer.
  */
 function getRandomInt(min: number, max: number) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+
+function attack
