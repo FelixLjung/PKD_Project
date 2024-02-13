67968d484ab1200132ae3271396b98b64905124b
FelixLjung <felix.ljung.4551@student.uu.se>
2024-02-13 13:29:05 +0100

    david

--- functions.ts
+++ functions.ts
@@ -112,12 +112,14 @@ export function attack(Attacking_army: Army, castle: Castle): Boolean {
     const Attack = enqueue_army(Attacking_army);
     const Defense = enqueue_army(defense_army);
     while(bool !== true){
-
+        break;
     }if(Attack[2].length === 0){      // If length of warrior queue is 0:
         return true; // temp return
     }else if(Defense[2].length === 0){
-    //    return false; // temp return
-    //}
+       return false; // temp return
+    }
+
+    return false;
 }
 
 /**
