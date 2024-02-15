db79dcd66fae4cd5f66bad5ccfe377a4f3689250
FelixLjung <felix.ljung.4551@student.uu.se>
2024-02-15 14:52:33 +0100

    print funktions

---
+++ Functions/print_functions.ts
@@ -0,0 +1,103 @@
+
+
+
+// Print functions 
+
+// start nodes
+let node1 = "1";
+let node2 = "2";
+let node5 = "5";
+
+//unclaimed nodes
+let node3 = "3x";
+let node4 = "4x";
+
+let nodes = [node1,node2,node3,node4,node5];
+
+let map = [
+    [" ", " ", " ", " ", nodes[0], " ", " ", " ", " "],
+    [" ", " ", "/", " ", "|", " ", "\\", " "],
+    [" ", "/", " ", " ", "|", " ", " ", "\\"],
+    [nodes[1], "-", "-", nodes[2], "-", "-", nodes[3]],
+    [" ", "\\", " ", " ", "|", " ", " ", "/", " "],
+    [" ", " ", "\\", " ", "|", " ", "/", "", " "],
+    [" ", " ", " ", " ", nodes[4], " ", " ", " ", " "]
+];
+
+
+export function refresh_board() {
+    
+    function get_castle_owners(){
+        for (let i = 0; i < nodes.length; i++ ){
+            //print(nodes[0])
+            nodes[i] = castles[i].position + castles[i].owner[0]; // fixa funktion för git castles
+        }
+
+        
+    }
+
+    /*
+    node1 = ((castles[0].position) as number) + castles[0].owner[0];
+    node1 = ((castles[0].position) as number) + castles[0].owner[0];
+    node1 = ((castles[0].position) as number) + castles[0].owner[0];
+    node1 = ((castles[0].position) as number) + castles[0].owner[0];
+    node1 = ((castles[0].position) as number) + castles[0].owner[0];
+    */
+
+    get_castle_owners();
+        
+
+    map = [
+        [" ", " ", " ", " ", nodes[0], " ", " ", " ", " "],
+        [" ", " ", "/", " ", "|", " ", "\\", " "],
+        [" ", "/", " ", " ", "|", " ", " ", "\\"],
+        [nodes[1], "-", "-", nodes[2], "-", "-", nodes[3]],
+        [" ", "\\", " ", " ", "|", " ", " ", "/", " "],
+        [" ", " ", "\\", " ", "|", " ", "/", "", " "],
+        [" ", " ", " ", " ", nodes[4], " ", " ", " ", " "]
+    ];
+}
+
+/**
+ * Prints the board to the console
+ * @param Array 2d array of the map
+ * @return Does not return
+ */
+
+export function print_board() {
+    refresh_board();
+    console.log("-------------------------------------------");
+    for (let i = 0; i < map.length; i++) {
+        console.log('\x1b[36m%s\x1b[0m', helper(map[i])); // black magic, Cyan Color
+    }
+
+    function helper(line: Array<string>) {
+        let str = "";
+
+        for (let j = 0; j < line.length; j++) {
+            str += line[j];
+        }
+        return str;
+    }
+    console.log("-------------------------------------------");
+}
+
+/**
+ * Gets an array of all the castles the player currently control.
+ * @param player the player in question.
+ * @returns Array<castle | undefined> of the castles
+ */
+export function get_castle(player: Player) {
+
+    let castles = player[1];
+    let print = "";
+
+
+    for(let i = 0; i < castles.length; i = i + 1 ){
+        print += castles[i]?.position;
+        print += " "
+
+    }
+    console.log(print);
+    console.log('\x1b[36m%s\x1b[0m',"You rule over the following castles: ", print, '\x1b[37m\x1b');
+}
