de8b3fb613668b8daf6b122108feba9a727150f2
David Julin <david.carl.julin@gmail.com>
2024-02-20 11:02:30 +0100

    dad

--- test/attack_function.test.ts
+++ test/attack_function.test.ts
@@ -1,6 +1,15 @@
 import { is_army_empty } from '../attack_functions'
 import { type Army } from '../types'
 
+//Mall för tests
+describe('funktionsnamn', () => {
+    it(' ', () => {
+        
+        }
+
+        expect().toEqual();
+    });
+
 
 it ('is_army_empty', () => {
     const empty_army : Army =
