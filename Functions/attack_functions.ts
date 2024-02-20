46f9a106adae6254306a9fec009081213fd68c56
FelixLjung <felix.ljung.4551@student.uu.se>
2024-02-20 16:46:29 +0100

    the rover

--- Functions/attack_functions.ts
+++ Functions/attack_functions.ts
@@ -1,7 +1,7 @@
 import { type Warrior, type Army, type Castle, type Player } from "../types";
 import { type Pair, tail, pair } from "../lib/list";
 import { type Queue, head, dequeue, enqueue, empty } from "../lib/queue_array";
-import { getRandomInt, get_castles } from "./general_functions";
+import { getRandomInt, get_order_castles} from "./general_functions";
 import { get_castle_array } from "./setup_functions";
 
 //Attack functions
@@ -82,7 +82,7 @@ export function castle_owner(castle : Castle, new_player : Player, old_player : 
     for (let i = 0; i < tail(old_player)!.length; i = i + 1) {
         if(tail(old_player)[i] == castle) {
             tail(old_player)[i] = undefined;
-            console.log(get_castles(old_player)[2]);
+            console.log(get_order_castles(old_player)[2]);
                 
             
         } else {
