384571452e93c79d639d25376d836d3bad845cc6
David Julin <david.carl.julin@gmail.com>
2024-02-20 11:29:53 +0100

    setup test

--- test/setup.test.ts
+++ test/setup.test.ts
@@ -1,6 +1,39 @@
+import {type Army, type Castle, type Warrior, type Player} from '../types';
+import { MatrixGraph } from '../lib/graphs';
+import { create_castle, create_warrior, create_army, get_castle_array, setup } from "../Functions/setup_functions";
+import { get_name } from '../Functions/general_functions';
+
 describe('', () => {
-    it('test 1: ', () => {
+    it('', () => {
         
+        }
+    
+        expect().toEqual();
+    });
+
+    // function create_warrior(): Warrior {
+    //let name = get_name();
+    
+    // const warrior = { attack: 5, health: 100, name: name };
+    // return warrior;
+    
+
+describe('create_warrior', () => {
+    it('creates a warrior', () => {
+        const name_test = "Erik Testsson"
+        let get_name = 
+
+        }
+        }
+
+        expect(create_warrior(, 10, 100)).toEqual();
+    });
+
+
+// function create_castle(army: Army, owner: string, position: number): Castle
+describe('create_castle', () => {
+    it('creates a castle', () => {
+        const army:Army = 
         }
 
         expect().toEqual();
