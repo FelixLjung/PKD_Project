fcc6e2db5e1563946a922b78791b44db134732c0
David Julin <david.carl.julin@gmail.com>
2024-03-04 16:25:57 +0100

    adad

--- Functions/general_functions.ts
+++ Functions/general_functions.ts
@@ -126,7 +126,6 @@ function remove_dead_castles(castles : Array<Castle | undefined>, player: Player
  * @param index 
  * @returns 
 */
-
 function get_position(castles: Array<Castle | undefined>, index: number): Castle | undefined {
     for (let i = 0; i < castles.length; i = i + 1) {
         if (castles[i] != undefined) {
@@ -232,6 +231,12 @@ export function finds_paths(castle: Castle, map: MatrixGraph): Array<number> {
 
     return paths;
 }
+
+/**
+ * 
+ * @param player_name 
+ * @returns 
+ */
 function get_player(player_name : String ) : Player | undefined {
     const player_array: Array<Player> = get_player_list();
 
@@ -243,6 +248,7 @@ function get_player(player_name : String ) : Player | undefined {
 
     return undefined
 }
+
 
 /**
  * Moves an army from one castle to another, attacking if it is an enemy castle
@@ -289,7 +295,7 @@ function move(move_from: Castle, move_to: Castle): void {
 }
 
 /**
- * Procecces the individual turn for a player
+ * Processes the individual turn for a player
  * @param player the player who will play a turn
  */
 
@@ -427,7 +433,7 @@ export function is_choice_in_paths(paths: Array<number>, choice: number): boolea
     }
 
 /**
- * Recruits a new warrior to a castle
+ * After every player has ended their turn, all castle recruits a new warrior.
  * @param castle - the castle which is recruiting the new warrior
  */
 export function recruit_warrior(castle: Castle, index: number) {
